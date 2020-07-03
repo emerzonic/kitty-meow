7d1975a33e04e6a17df4e8af6f8589a34cde1120
Emerson Doyah <emerson.doyah@gmail.com>
2020-07-03 15:53:40 -0500

    Updates

---
+++ precache-manifest.e1066e387cdb8d0687bf843330db8a07.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72f6c63044b00f2a6d386d7ddea94222",
+    "url": "/kitty-meow/index.html"
+  },
+  {
+    "revision": "b00a7009e6ca688e9256",
+    "url": "/kitty-meow/static/css/main.27ed47ea.chunk.css"
+  },
+  {
+    "revision": "ee89fd8b69e7a58b9281",
+    "url": "/kitty-meow/static/js/2.8f226866.chunk.js"
+  },
+  {
+    "revision": "56729d980da09961afe8d497a21eec12",
+    "url": "/kitty-meow/static/js/2.8f226866.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b00a7009e6ca688e9256",
+    "url": "/kitty-meow/static/js/main.a4735d7e.chunk.js"
+  },
+  {
+    "revision": "78d2be3f631a6a064a47",
+    "url": "/kitty-meow/static/js/runtime-main.ecf9bf17.js"
+  }
+]);
